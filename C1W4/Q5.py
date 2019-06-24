20175b02ff28197cad6e1b58da40e2fefbf2583a
Shibo <ssb@hust.edu.cn>
2019-06-24 17:16:50 +0200

    Create Q5.py

---
+++ C1W4/Q5.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 26 15:18:33 2019
+
+@author: Shibo
+"""
+import pandas as pd
+import numpy as np
+states = {'OH': 'Ohio', 'KY': 'Kentucky', 'AS': 'American Samoa', 'NV': 'Nevada', 'WY': 'Wyoming', 'NA': 'National', 'AL': 'Alabama', 'MD': 'Maryland', 'AK': 'Alaska', 'UT': 'Utah', 'OR': 'Oregon', 'MT': 'Montana', 'IL': 'Illinois', 'TN': 'Tennessee', 'DC': 'District of Columbia', 'VT': 'Vermont', 'ID': 'Idaho', 'AR': 'Arkansas', 'ME': 'Maine', 'WA': 'Washington', 'HI': 'Hawaii', 'WI': 'Wisconsin', 'MI': 'Michigan', 'IN': 'Indiana', 'NJ': 'New Jersey', 'AZ': 'Arizona', 'GU': 'Guam', 'MS': 'Mississippi', 'PR': 'Puerto Rico', 'NC': 'North Carolina', 'TX': 'Texas', 'SD': 'South Dakota', 'MP': 'Northern Mariana Islands', 'IA': 'Iowa', 'MO': 'Missouri', 'CT': 'Connecticut', 'WV': 'West Virginia', 'SC': 'South Carolina', 'LA': 'Louisiana', 'KS': 'Kansas', 'NY': 'New York', 'NE': 'Nebraska', 'OK': 'Oklahoma', 'FL': 'Florida', 'CA': 'California', 'CO': 'Colorado', 'PA': 'Pennsylvania', 'DE': 'Delaware', 'NM': 'New Mexico', 'RI': 'Rhode Island', 'MN': 'Minnesota', 'VI': 'Virgin Islands', 'NH': 'New Hampshire', 'MA': 'Massachusetts', 'GA': 'Georgia', 'ND': 'North Dakota', 'VA': 'Virginia'}
+data5 = pd.read_csv("City_Zhvi_AllHomes.csv")
+house1=data5.iloc[:,1:3]
+house2=data5.iloc[:,51:]
+#house=pd.merge(house1,house2,how="inner",left_index=True,right_index=True)
+house=pd.merge(house1,house2,how="inner",left_index=True,right_index=True)
+house.replace({'State':states}, inplace = True)
+house=house.set_index(["State","RegionName"])
+house = house.groupby(pd.PeriodIndex(house.columns, freq='Q'), axis=1).mean()
